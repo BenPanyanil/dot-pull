f51198663510beba719838a93e8b98309438720b
Ben Panyanil <panyanilben@gmail.com>
2022-06-17 22:28:21 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvas id="canvas" :width="width" :height="height" style="border:2px solid #dddddd;"></canvas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      width: 1000,
+      height:500,
+
+      pos_x: 100,
+      pos_y: 100,
+      radius: 20
+    }
+  },
+  mounted: function () {
+    // this.animation();
+    requestAnimationFrame(this.draw)
+  },
+  methods: {
+    animation: function () {
+      requestAnimationFrame(this.draw);
+    },
+    draw: function () {
+      const c = document.getElementById("canvas");
+      const ctx = c.getContext("2d");
+
+      ctx.clearRect(0, 0, this.width, this.height);
+
+      ctx.beginPath();
+      ctx.arc(this.pos_x, this.pos_y, this.radius, 0, Math.PI * 2);
+      ctx.stroke();
+
+      this.pos_x += 1
+
+      requestAnimationFrame(this.draw)
+    },  
   }
 }
 </script>
